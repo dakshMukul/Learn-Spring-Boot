29fa58aadfe8b3efcc2ef99bc184cc25de5fde16
dakshMukul <105584413+dakshMukul@users.noreply.github.com>
2023-08-18 01:04:05 +0530

    Initial Commit

---
+++ BuildRestApi/src/main/java/com/springboot/restApi/BuildRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.restApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildRestApiApplication.class, args);
+	}
+
+}
